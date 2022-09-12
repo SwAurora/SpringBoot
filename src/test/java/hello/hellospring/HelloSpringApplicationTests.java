cc91799678ad65945b4e9752e8e6b0b88529557f
SwAurora <rlatjddnr74@gmail.com>
2022-09-12 15:56:39 +0900

    2022-09-12 master(ver1)

--- src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class HelloSpringApplicationTests
+{
+    @Test
+    void contextLoads()
+    {
+    }
 
 }
