193e870e53e0e46085bcbc1ca9eb7c87336daaef
SwAurora <rlatjddnr74@gmail.com>
2022-09-14 00:00:31 +0900

    2022-09-14 master(ver1)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +27,6 @@ public class SpringConfig
     @Bean
     public MemberRepository memberRepository()
     {
-        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
